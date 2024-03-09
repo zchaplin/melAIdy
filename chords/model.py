a484cff4db82d00e5b53298a38144d81be84af97
MikaPS <mikaps7@gmail.com>
2024-03-08 17:39:17 -0800

    deleted some comments

--- chords/model.py
+++ chords/model.py
@@ -19,56 +19,34 @@ train_answer = np.array(train_answer_padded)
 test = np.array(test_padded)
 test_answer = np.array(test_answer_padded)
 
-# [THIS IS A BUG: SIZE SHOULD ALWAYS BE THE SAME. IT'S A PROBLEM WITH THE CREATE_DATASETS.PY BUT I WANTED TO CHECK IF THIS DIRECTION IS GOOD BEFORE FIXING BUGS]
-# same size between x and y
-min_samples = min(len(train), len(train))
-train = train[:min_samples]
-train_answer = train_answer[:min_samples]
-test = test[:min_samples]
-test_answer = test_answer[:min_samples]
-
 # One-hot encode the labels
-train_one_hot = to_categorical(train)
-test_one_hot = to_categorical(test)
-
 train_answer_one_hot = to_categorical(train_answer)
 test_answer_one_hot = to_categorical(test_answer)
 
-
-print(f"train: {train}\n\ntrain answer: {train_answer}\n\ntest: {test}\n\ntest answer: {test_answer}")
+# print(f"train: {train}\n\ntrain answer: {train_answer}\n\ntest: {test}\n\ntest answer: {test_answer}")
 num_classes = test_answer_one_hot.shape[1]
 input_shape = (len(train[0]),)
 print("num classes:" , num_classes)
 
 # Define the model
-
 model = Sequential()
-
 # model.add(Rescaling(1/255, input_shape=input_shape))
-# model.add(Dense(32, activation='relu'))
-# model.add(Conv2D(64,kernel_size=(3, 3), dilation_rate=(2, 2), activation='relu', padding='same'))
-# model.add(DepthwiseConv2D(kernel_size=(3, 3), activation='relu', padding='same'))
-# model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-# model.add(Conv2D(32,kernel_size=(2,2)))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Conv2D(32,kernel_size=(3, 3), activation='relu', padding='same'))
-# model.add(MaxPooling2D())
-
 model.add(Reshape((1, 2, 1), input_shape=(2,)))  # reshape it so we could use the Conv2D
 model.add(Conv2D(filters=32, kernel_size=(1, 1), activation='relu'))
 model.add(Conv2D(num_classes,(1,1), activation='relu'))
-model.add(MaxPooling2D(pool_size=(1, 1)))
-model.add(SpatialDropout2D(0.1))
+model.add(DepthwiseConv2D(kernel_size=(1, 1), activation='relu', padding='valid'))
+model.add(Conv2D(32,kernel_size=(1,1)))
+model.add(Conv2D(32,kernel_size=(1, 1), activation='relu'))
+
+model.add(SpatialDropout2D(0.01))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.1))
 model.add(Dense(32, activation='relu'))
 model.add(BatchNormalization())
 
-# model.add(Embedding(input_dim=num_classes, output_dim=50, input_length=2))
-
 model.add(Dense(64, activation='relu'))
 model.add(Flatten())
-# model.add(Dense(256, activation='relu'))
+model.add(Dense(256, activation='relu'))
 
 model.add(Dense(num_classes, activation='softmax'))
 
@@ -83,9 +61,6 @@ model.compile(
 model.fit(train, train_answer_one_hot, epochs=20, batch_size=32, validation_data=(train, train_answer_one_hot))
 
 # Evaluate the model
-print("Shape of test:", test.shape)
-print("Shape of test_answer_one_hot:", test_answer_one_hot.shape)
-
 loss, accuracy = model.evaluate(test, test_answer_one_hot)
 print(f'Test Loss: {loss}, Test Accuracy: {accuracy}')
 
